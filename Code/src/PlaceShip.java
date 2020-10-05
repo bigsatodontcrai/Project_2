f6707b1a64b0f7e33541785bdaf461ed7cf46a88
Valerie Hernandez <v134h265@ku.edu>
2020-10-04 22:46:46 -0500

    placeShip didn't make last push

--- Code/src/PlaceShip.java
+++ Code/src/PlaceShip.java
@@ -1,11 +1,20 @@
 import java.lang.IllegalArgumentException;
 
+/**
+ * Note for others: The PlaceShip class allows for the user, or AI, to place their ships on the 
+ * board
+ */
 public class PlaceShip {
     private Board board;
     private String shipNumber;
     private boolean isHori;
     private boolean directionSucceeded;
 
+    /**
+     * PlaceShip Constructor
+     * @param theBoard - board object
+     * @return PlaceShip object
+     */ 
     public PlaceShip(Board theBoard){
         this.board = theBoard;
         this.isHori = false;
@@ -14,7 +23,11 @@ public class PlaceShip {
         
     }
    
-        
+    /**
+     * sets the direction that the ship will extend to after placing the ship's head
+     * @param ds - boolean 
+     * @return boolean - ds  
+     */  
     private boolean setShipDirection(boolean ds) {
         directionSucceeded = ds;
         return directionSucceeded;
@@ -33,6 +46,12 @@ public class PlaceShip {
         return true;
     }*/
 
+    /**
+     * actual placing of the ship and making sure the ship in in bounds of the board
+     * @param row - int that holds number of rows, col - int that holds number of columns,
+     *  size - int that holds the size of the row/col
+     * @return boolean - returns false if any ship coordinates fall out of bounds
+     */
     private boolean placeIterative(int row, int col, int size) {
         boolean[] check = new boolean[size];
         if(isHori) {
@@ -71,10 +90,22 @@ public class PlaceShip {
         
     }
     
+    /**
+     * calls placeIterative method to place a ship
+     * @param row - int that holds number of rows, col - int that holds number of columns,
+     *  size - int that holds the size of the row/col
+     * @return boolean - returns the results of the bool type placeIterative
+     */
     private boolean placeIt(int row, int col, int size){
         return placeIterative(row, col, size);
     }
 
+    /**
+     * places ship on board and checks if ship will overlap other ships
+     * @param row - int that holds number of rows, col - int that holds number of columns,
+     *  size - int that holds the size of the row/col, dir - boolean
+     * @return boolean - returns false if newly placed ship overlaps with another ship
+     */
     public boolean place(int row, int col, int size, boolean dir) {
         try {
             isHori = dir;
@@ -89,6 +120,11 @@ public class PlaceShip {
         }
     }
 
+    /**
+     * grabs ship label
+     * @param none
+     * @return string - the ships label
+     */
     public String getShipName() {
         return shipNumber;
     }
